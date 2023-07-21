136b44c2fab2b08cd42222d5f8eb1711939a27d3
Yurii Romanenko <poligrafist.biz@gmail.com>
2023-07-21 15:05:49 +0300

    add Shared Preferenced

--- app/src/main/java/yurii/romanenko/shpp/AuthActivity.kt
+++ app/src/main/java/yurii/romanenko/shpp/AuthActivity.kt
@@ -1,8 +1,8 @@
 package yurii.romanenko.shpp
 
 import android.app.ActivityOptions
+import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,23 +13,39 @@ import kotlin.properties.Delegates.notNull
 
 class AuthActivity : AppCompatActivity() {
 
+    private val SHARED_PREFS_EMAIL = "put_email"
+    private val SHARED_PREFS_PASSWORD = "put_password"
     private lateinit var binding: AuthLayoutBinding
     private var email by notNull<String>()
-    private var pass by notNull<String>()
+    private var password by notNull<String>()
     private var emailValid: Boolean = false
     private var passwordValid: Boolean = false
-    private lateinit var settings : SharedPreferences
+
+
+       val sharedPrefs by lazy {
+           getSharedPreferences(
+               "shared_preference",
+               Context.MODE_PRIVATE
+           )
+       }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = AuthLayoutBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
+        email = sharedPrefs.getString(SHARED_PREFS_EMAIL, "yuri.romanenko").toString()
+        password = sharedPrefs.getString(SHARED_PREFS_PASSWORD, "KotlinMyLov").toString()
+
+        binding.textEditEmail.setText(email)
+        binding.textEditPass.setText(password)
+
         emailCheck()
         passwordCheck()
 
         binding.buttonRegister.setOnClickListener() { clickButtonRegister() }
-     //   renderState()
     }
+
 
     private fun passwordCheck() {
         binding.textEditPass.doOnTextChanged { text, start, before, count ->
@@ -54,31 +70,29 @@ class AuthActivity : AppCompatActivity() {
     }
 
     private fun clickButtonRegister() {
-        val intent = Intent(this, ProfileActivity::class.java)
+        val intentProfileActivity = Intent(this, ProfileActivity::class.java)
         val optionTransitionAnimation = ActivityOptions.makeSceneTransitionAnimation(this)
+        var remembeMeIsChecked : Boolean = binding.checkBoxRememberMe.isChecked
         email = binding.textEditEmail.text.toString()
-        pass = binding.textEditPass.text.toString()
+        password = binding.textEditPass.text.toString()
         Log.d("TESTLOG", "Click REGISTER: $email")
         if (!emailValid) {
             Toast.makeText(this, "Email '$email' is not valid", Toast.LENGTH_SHORT).show()
         } else if (!passwordValid) {
-            Toast.makeText(this, "Password '$pass' is not valid", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Password '$password' is not valid", Toast.LENGTH_SHORT).show()
         } else {
-            startActivity(intent, optionTransitionAnimation.toBundle())
 
-            intent.putExtra("text_name", parseEmailToName(email))
-         //   renderState()
+            if (remembeMeIsChecked){
+            sharedPrefs.edit().putString(SHARED_PREFS_EMAIL, email).apply()
+            sharedPrefs.edit().putString(SHARED_PREFS_PASSWORD, password).apply()
+         }
+
+            intentProfileActivity.putExtra("text_name", parseEmailToName(email))
+            startActivity(intentProfileActivity, optionTransitionAnimation.toBundle())
+
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-      //  email = binding.textEditEmail.text.toString()
-//        outState.putString(KEY_EMAIL, email)
-//        outState.putString(KEY_PASS, pass)
-//        renderState()
-    }
 
     private fun parseEmailToName(email: String): String {
         var secondName: String = email.substringBefore('@')
@@ -86,18 +100,6 @@ class AuthActivity : AppCompatActivity() {
         return firstName + " " + secondName.substringAfter('.').capitalize()
     }
 
-//    fun renderState() = with(binding) {
-//        textEditEmail.setText(email)
-//        textEditPass.setText(pass)
-//    }
-//
-//    companion object {
-//        @JvmStatic
-//        private val KEY_EMAIL = "EMAIL"
-//
-//        @JvmStatic
-//        private val KEY_PASS = "PASS"
-//    }
 }
 
 
