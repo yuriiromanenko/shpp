44eae22eb962d486482eb921d23d613a9b60155f
Yurii Romanenko <poligrafist.biz@gmail.com>
2024-01-20 15:47:27 +0200

    add Navigation rename app

---
+++ app/src/main/java/ua/shpp/yurom/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.shpp.yurom.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.shpp.yurrom.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
